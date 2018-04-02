564af968c1b4a697f4979d1a402230a7f0740d08
yogicat <dahe.oh@gmail.com>
2018-04-02 16:00:18 +0900

    edit routing and layout

---
+++ src/app/restaurants/restaurants-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RestaurantsComponent } from './restaurants.component';
+
+
+const routes: Routes = [
+  { path: 'restaurants', component: RestaurantsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RestaurantRoutingModule { }
--- src/app/restaurants/restaurants.module.ts
+++ src/app/restaurants/restaurants.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RestaurantsComponent } from './restaurants.component';
+
+import { RestaurantRoutingModule } from './restaurants-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RestaurantRoutingModule
   ],
   declarations: [
     RestaurantsComponent
