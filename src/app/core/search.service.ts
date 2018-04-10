6edb4ecbe9e621183db55d86a6344b098af9ff2d
yogicat <dahe.oh@gmail.com>
2018-04-10 15:03:33 +0900

    address search service setting

--- src/app/core/search.service.ts
+++ src/app/core/search.service.ts
@@ -4,25 +4,30 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+
 interface SearchResult {
    search_text: string;
    result: any[];
-}
-
-interface SearchInput {
-  search_text: string;
 }
 
 @Injectable()
 export class SearchService {
   URL = 'https://www.overeats.kr/api';
 
-
   constructor(private http: HttpClient) { }
 
+  search(terms: Observable<string>) {
+    return terms.debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap(term => this.searchAddress(term));
+  }
 
-  search(text: string): Observable<SearchResult> {
-    return this.http.post<SearchResult>(`${this.URL}/address/`, {search_text: text}, {
+  searchAddress(term) {
+    return this.http.post<SearchResult>(`${this.URL}/address/`, { search_text: term }, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
@@ -31,3 +36,6 @@ export class SearchService {
 
 
 }
+
+
+
--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,19 +1,11 @@
-import { Component, OnInit, Input, ElementRef, ViewChild, OnDestroy } from '@angular/core';
-import { NgModel, FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
 import { Router } from '@angular/router';
 import { SearchService } from '../../core/search.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
-
-// Observable operators
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/of';
-
+import { Subject } from 'rxjs/Subject';
 
 interface SearchResult {
   search_text: string;
@@ -24,86 +16,77 @@ interface SearchResult {
   selector: 'app-search',
   styleUrls: ['./search.component.scss'],
   template: `
-    <div
+  <div
   class="search-group"
   [ngClass]="{'onHeader': onMenu, 'onMain': !onMenu}">
   <div class="input-group">
     <label>
       <i class="fas fa-map-marker-alt"></i>
-      <input 
+      <input
         type="text"
         class="input uber input-search"
         name="searchAddress"
-        [formControl]="searchInput"
-        placeholder="배달 주소 입력 *도로명/지번주소 + 번지수 기입 필수* ">
+        (keyup)="searchAddress($event.target.value)"
+        (keydown)="onKeyDown($event)"
+        placeholder="배달 주소를 입력하세요">
       <ul
         *ngIf="addresses"
         class="search-list">
         <li class="button"
-          *ngFor="let address of addresses"
+          *ngFor="let address of addresses; let i = index"
+          [class.active] = "i == arrowKeyLocation"
           (click)="sendAddress(address)">
           <em>{{ address.formatted_address }}</em>
         </li>
       </ul>
     </label>
-    <button
-      class="button uber button-main hideOnMobile"
-      *ngIf="!onMenu"
-      (click)="onEnter()">음식 찾기</button>
   </div>
 </div>
   `
 })
-export class SearchComponent implements OnInit, OnDestroy {
+export class SearchComponent implements OnInit {
 
   @Input()
   onMenu: boolean;
 
-  value = '';
-  searchInput = new FormControl('');
+  arrowKeyLocation = 0;
+  searchTerm$ = new Subject<string>();
   addresses: any;
-  subscription: Subscription;
 
   constructor(
     private router: Router,
     private searchService: SearchService
-  ) { }
-
-  ngOnInit() {
-    this.subscription = this.searchInput.valueChanges
-        .debounceTime(1000)
-        .switchMap(input => this.getAddress(input))
-        .subscribe(address => {
-          this.addresses = address.result;
+  ) {
+      this.searchService.search(this.searchTerm$)
+        .subscribe(results => {
+          this.addresses = results.result;
           console.log(this.addresses);
         });
   }
 
-  onSearch() {
-    if (this.value.length > 1) {
-      this.searchService.search(this.value)
-        .subscribe(
-          (data) => {
-            this.addresses = data.result;
-            console.log(data.result);
-          }
-        );
+  ngOnInit() { }
+
+  searchAddress(value) {
+    if (value && value.length > 1) {
+      this.searchTerm$.next(value);
     }
   }
+
   sendAddress(address) {
-    this.router.navigate(['/menu', address.place_id]);
-  }
-  onEnter() {
-    this.router.navigate(['/menu', 123]);
+    this.router.navigate(['menu', address.place_id]);
   }
 
-  getAddress(address: string): Observable<SearchResult> {
-    return this.searchService.search(address);
+  onKeyDown(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'ArrowDown':
+        this.arrowKeyLocation++;
+        break;
+      case 'ArrowUp':
+        this.arrowKeyLocation--;
+        break;
+      default:
+        break;
+    }
   }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
 
 }
