6817bff12289bb0d34616ea821ba5a901c61d021
yogicat <dahe.oh@gmail.com>
2018-04-14 00:46:34 +0900

    delete unused files

--- src/app/models/restaurant.ts
+++
@@ -1,9 +0,0 @@
-export interface Restaurant {
-    id: number;
-    name: string;
-    image: string;
-    address: string;
-    open: boolean;
-    deliveryMin: number;
-    foodTypes: string[];
-}
