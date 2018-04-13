4b653ccb39e61f7c2a3801830415531343232881
yogicat <dahe.oh@gmail.com>
2018-04-14 00:41:38 +0900

    minor fix

--- src/app/shared/preloader/preloader.component.ts
+++ src/app/shared/preloader/preloader.component.ts
@@ -5,7 +5,7 @@ import { PreloaderService } from './preloader.service';
   selector: 'app-preloader',
   styleUrls: ['./preloader.component.scss'],
   template: `
-  <div class="showbox" [ngClass]="{'onHeader': onMenu}">
+  <div class="showbox" [ngClass]="{'onHeader': onMenu, 'onMain': !onMenu}">
     <div class="loader">
       <svg class="circular" viewBox="25 25 50 50">
         <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
