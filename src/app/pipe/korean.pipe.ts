e7a3d79d1ccca3b19b9b81ae0654004985ee741a
yogicat <dahe.oh@gmail.com>
2018-04-18 22:15:53 +0900

    edit regex

--- src/app/pipe/korean.pipe.ts
+++ src/app/pipe/korean.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class KoreanPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    value = value.replace(/[a-zA-Z]+./g, '');
+    value = value.replace(/[a-zA-Z].*/g, '');
     return value;
   }
 }
