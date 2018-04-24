dc32e89e838012cdd63422cfb214300ab541026f
bluelion2 <iltk2000@gmail.com>
2018-04-24 14:30:14 +0900

    user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,7 +3,7 @@ import { AuthService } from '../auth/services/auth.service';
 import { User } from '../auth/user';
 import { environment } from '../../environments/environment';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, controlNameBinding } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-user',
@@ -19,51 +19,54 @@ import { ActivatedRoute } from '@angular/router';
         <p> 기본 정보 </p>
         <div class="user-name user"> 유저 이름
           <input type="text"
-          formControlName="username">
-          <span class="error" *ngIf="usernameFormat">
-            이메일을 입력해 주세요.
-          </span>
-          <span class="error" *ngIf="usernamePattern">
-            이메일 형식에 맞게 입력해 주세요.
-          </span>
+          formControlName="username" disabled>
         </div>
-        <div class="user"> 이메일 :
+        <div class="user-password user"> 비밀 번호
           <input type="text"
-          formControlName="email">
-          <span class="error" *ngIf="usernameFormat">
-            이메일을 입력해 주세요.
+          formControlName="password">
+          <ng-container *ngIf="password.invalid && (password.dirty || password.touched)">
+          <span class="error" *ngIf="password.errors.required">
+            비밀번호를 입력해주세요.
           </span>
-          <span class="error" *ngIf="usernamePattern">
-            이메일 형식에 맞게 입력해 주세요.
-          </span>
+          </ng-container>
         </div>
         <div class="name user">
           <div class="last-name"> 성
             <input type="text"
             formControlName="last_name">
-            <span class="error" *ngIf="lastNameInvalid">
+            <ng-container *ngIf="lastName.invalid && (lastName.dirty || lastName.touched)">
+            <span class="error" *ngIf="lastName.errors.required">
               성을 입력해 주세요.
             </span>
+            <span class="error" *ngIf="lastName.errors.pattern">
+              영문을 입력해주세요.
+            </span>
+            </ng-container>
           </div>
           <div class="first-name"> 이름
             <input type="text"
             formControlName="first_name">
-            <span class="error" *ngIf="firstNameInvalid">
-              이름을 입력해 주세요.
-            </span>
+            <ng-container>
+              <span class="error" *ngIf="firstName.errors.required">
+                이름을 입력해 주세요.
+              </span>
+              <span class="error" *ngIf="firstName.errors.pattern">
+                영문을 입력해 주세요.
+              </span>
+            </ng-container>
           </div>
         </div>
         <div class="phone-number user">휴대전화 번호
           <input type="text"
             formControlName="phone_number">
-            <span class="error" *ngIf="phoneNumberInvalid">
+            <span class="error" *ngIf="phoneNumber.errors.required">
             전화번호를 입력해 주세요.
           </span>
         </div>
 
       </div>
     </div>
-    <div class="save">
+    <div class="save" *ngIf="this.userform.dirty">
       <button type="submit"> 저장하기 </button>
     </div>
     <pre>{{ this.userform.value | json}}</pre>
@@ -103,17 +106,18 @@ export class UserComponent implements OnInit {
 
   makeForm() {
     this.userform = this.fb.group({
-      username: [this.user.username, [
-        Validators.required,
-        Validators.pattern(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/)
-      ]],
-      email: [this.user.username, [
-        Validators.required,
-        Validators.pattern(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/)
+      username: [this.user.username, {disabled: true}],
+      email: [this.user.email, {disabled: true}],
+      password: ['', [
+      Validators.required,
+      Validators.pattern(/[a-zA-Z0-9]/),
+      Validators.minLength(5),
+      Validators.maxLength(10)
       ]],
       first_name: [this.user.first_name, [
         Validators.required,
         Validators.pattern(/[a-zA-z]/),
+        Validators.minLength(1)
       ]],
       last_name: [this.user.last_name, [
         Validators.required,
@@ -128,41 +132,45 @@ export class UserComponent implements OnInit {
     });
   }
   saveUser() {
-    // this.auth.updateUser(this.userform.value, this.pk, this.token)
-    //   .subscribe(data => console.log('update user', data));
+    this.auth.updateUser(this.userform.value, this.pk, this.token)
+      .subscribe(data => console.log('update user', data));
     console.log(this.userform.value);
   }
 
-  get usernameFormat() {
-    const control = this.userform.get('username');
-    return control.hasError('required') && control.touched;
+
+  // get passwordInvalid() {
+  //   const control = this.userform.get('password');
+  //   return control.hasError('required');
+  // }
+
+  // get firstNameInvalid() {
+  //   const control = this.userform.get('first_name')
+  //   return control.hasError('required');
+  // }
+
+  // get lastNameInvalid() {
+  //   const control = this.userform.get('last_name');
+  //   return control.hasError('required');
+  // }
+
+  // get phoneNumberInvalid() {
+  //   const control = this.userform.get('');
+  //   return control.hasError('required');
+  // }
+
+  get password() {
+    return this.userform.get('password');
   }
-  get usernamePattern() {
-    const control = this.userform.get('username');
-    return control.hasError('pattern') && control.touched;
-  }
-  get useremailFormat() {
-    const control = this.userform.get('email');
-    return control.hasError('required') && control.touched;
-  }
-  get useremailPattern() {
-    const control = this.userform.get('email');
-    return control.hasError('pattern') && control.touched;
+  get firstName() {
+    return this.userform.get('first_name');
   }
 
-  get firstNameInvalid() {
-    const control = this.userform.get('first_name');
-    return control.hasError('pattern') && control.touched;
+  get lastName() {
+    return this.userform.get('last_name');
   }
 
-  get lastNameInvalid() {
-    const control = this.userform.get('last_name');
-    return control.hasError('pattern') && control.touched;
-  }
-
-  get phoneNumberInvalid() {
-    const control = this.userform.get('phone_number');
-    return control.hasError('pattern') && control.touched;
+  get phoneNumber() {
+    return this.userform.get('phone_number');
   }
 
 }
