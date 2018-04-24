650fac450b8c7102630e59446d29b17faa2dae8d
bluelion2 <iltk2000@gmail.com>
2018-04-25 02:04:25 +0900

    make "get location()" for lat, lng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from '../core/cart.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,33 @@ import { CartService } from '../core/cart.service';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   onMain = true;
+  lat: string;
+  lng: string;
   constructor(
+    private http: HttpClient,
+    public router: Router
     // private cartService: CartService
   ) { }
 
   ngOnInit() {
     // this.cartService.emptyCart();
+  }
+
+
+  getLocation(location: string) {
+    this.http.get<any>('https://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: location,
+        key: 'AIzaSyCWNHncRx3LgQURlXOHMkN_vbpQxZGqISU'
+      }
+    })
+    .subscribe((res) => {
+      this.lat = (res.results[0].geometry.location.lat).toString();
+      this.lng = (res.results[0].geometry.location.lng).toString();
+      console.log('lat', this.lat, 'lng', this.lng);
+      this.router.navigate(['/restaurants', `${this.lat}`, `${this.lng}`]);
+      }
+    );
   }
 
   ngOnDestroy() {
